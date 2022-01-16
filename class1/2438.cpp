166f0bc24876028a58a444e2853a9c7aa4d7fa41
yoon-H <ucando0320@gmail.com>
2022-01-16 22:09:59 +0900

    Add 2438.cpp

--- class1/2438.cpp
+++ class1/2438.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int main() {
+
 	int n{ 0 };
 
 	cin >> n;
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j <= i; j++)
-		{
 			cout << "*";
-		}
+
 		cout << endl;
 	}
 }
