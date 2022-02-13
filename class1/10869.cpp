167faa12e2f3991e984c70540d47b5693dc258e0
yoon-H <ucando0320@gmail.com>
2022-02-13 23:17:34 +0900

    Add 10869.cpp

---
+++ class1/10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a{ 0 };
+	int b{ 0 };
+
+	cin >> a >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b ;
+}
